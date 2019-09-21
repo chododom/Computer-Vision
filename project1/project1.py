82a6323805e6c73e7b7f4363faa5540399eaf3cc
chododom <chododom@fit.cvut.cz>
2019-09-20 21:04:42 -0400

    Split image array into separate channel arrays

--- project1/project1.py
+++ project1/project1.py
@@ -4,16 +4,8 @@ import matplotlib.pyplot as plt
 import cv2
 
 
-
+# given a filename, function returns array containing data aquired from an image in PPM format
 def loadppm(filename):
-    '''Given a filename, return a numpy array containing the ppm image
-    input: a filename to a valid ascii ppm file 
-    output: a properly formatted 3d numpy array containing a separate 2d array 
-            for each color
-    notes: be sure you test for the correct P3 header and use the dimensions and depth 
-            data from the header
-            your code should also discard comment lines that begin with #
-    '''
     file = open(filename, "r")
     
     case = 0
@@ -26,6 +18,7 @@ def loadppm(filename):
         if line == "":
             break
         arr = line.split()
+        # ignore comment lines
         if arr[0] == "#":
             continue
         elif case == 0:
@@ -37,6 +30,7 @@ def loadppm(filename):
         elif case == 2:
             max_val = arr[0]
         else:
+            # load actual image pixels
             for i in range(len(arr)):
                 img[(x, y, rgb)] = arr[i]
                 rgb += 1
@@ -48,21 +42,20 @@ def loadppm(filename):
                     x += 1
         case += 1
     return img;
-  # return the numpy 3d array
-
 
 
 def GetGreenPixels(img):
     '''given a numpy 3d array containing an image, return the green channel'''
-    pass
+    return img[:,:,1]
 
 def GetBluePixels(img):
     '''given a numpy 3d array containing an image, return the blue channel'''
-    pass
+    return img[:,:,2]
 
 def GetRedPixels(img):
     '''given a numpy 3d array containing an image, return the red channel'''
-    pass
+    return img[:,:,0]
+
 
 
 if __name__ == "__main__":
