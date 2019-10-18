8b9f6d018bac805ea1e58d34d50f3d692f670693
chododom <chododom@fit.cvut.cz>
2019-10-18 01:07:44 -0400

    Non max suppression seems to work, but visualization draws circles wrong (when drawing just pixels, it's fine)

--- project4/project4.py
+++ project4/project4.py
@@ -1,6 +1,13 @@
 import cv2
 import numpy as np
 
+# applies linearly separable Gaussian filters
+def gaussianThatShit(img):
+    gaussian = cv2.getGaussianKernel(ksize=5, sigma=3)
+    img = cv2.filter2D(img, cv2.CV_16S, gaussian)
+    img = cv2.filter2D(img, cv2.CV_16S, gaussian.T)
+    return img
+
 # Harris detection function to assign R-values to pixels which are considered to be edges
 def HarrisDetector(img,k = 0.04):
     grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -17,9 +24,9 @@ def HarrisDetector(img,k = 0.04):
     Iyy = Iy ** 2
     Ixy = Ix * Iy
     
-    g_Ixx = cv2.filter2D(Ixx, cv2.CV_16S, gaussian)
-    g_Iyy = cv2.filter2D(Iyy, cv2.CV_16S, gaussian)
-    g_Ixy = cv2.filter2D(Ixy, cv2.CV_16S, gaussian)
+    g_Ixx = gaussianThatShit(Ixx)
+    g_Iyy = gaussianThatShit(Iyy)
+    g_Ixy = gaussianThatShit(Ixy)
     
     height = grey.shape[0]
     width = grey.shape[1]
@@ -34,19 +41,38 @@ def HarrisDetector(img,k = 0.04):
 
 
 def SuppressNonMax(Rvals, numPts):
-    '''
-    Args:
+    height = len(Rvals)
+    width = len(Rvals[0])
     
-    -   Rvals: A numpy array of shape (m,n,1), containing Harris response values
-    -   numPts: the number of responses to return
+    r_vals = np.zeros( height * width, dtype=([('x', int), ('y', int), ('r', float), ('sup_r', float)]) )
+    
+    index = 0
+    max_r = Rvals.max()
+    for i in range(height):
+        for j in range(width):
+            if Rvals[i][j] > 0.01 * max_r:
+                r_vals[index] = (i, j, Rvals[i][j], 0)
+                index += 1
 
-    Returns:
-
-     x: A numpy array of shape (N,) containing x-coordinates of interest points
-     y: A numpy array of shape (N,) containing y-coordinates of interest points
-     confidences (optional): numpy nd-array of dim (N,) containing the strength
-            of each interest point
-   '''
-    pass 
-
-
+    r_vals = r_vals[:index]
+    
+    r_vals = (np.sort(r_vals, order='r'))[::-1]   
+    r_vals[0][3] = np.inf
+    
+    for i in range(1, r_vals.shape[0]):
+        min_sup_r = np.inf
+        for j in range(0, i):
+            distance = np.hypot(r_vals[j][0], r_vals[j][1])
+            if distance < min_sup_r:
+                min_sup_r = distance
+        r_vals[i][3] = min_sup_r
+    
+    r_vals = (np.sort(r_vals, order='sup_r'))[::-1]
+    x_vals = np.zeros(numPts)
+    y_vals = np.zeros(numPts)
+    
+    for i in range(numPts):
+        x_vals[i] = r_vals[i][0]
+        y_vals[i] = r_vals[i][1]
+        
+    return x_vals, y_vals
